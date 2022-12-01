4b580db69560585960e56bfa72e5c03d16383e7d
Fredric Ryding <fredricr91@hotmail.com>
2022-12-01 16:25:23 +0100

    Added react-cookie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg'
 import './App.css'
 
+import { useCookies } from 'react-cookie'
+
 function App() {
+  const [cookies, setCookie] = useCookies(['name'])
+
+  function onChange() {
+    let expireDate = new Date()
+    expireDate.setTime(expireDate.getTime() + 1 * 3600 * 1000)
+
+    setCookie('name', 'fr3dz', {
+      expires: expireDate,
+      httpOnly: false,
+      path: '/',
+      secure: true,
+    })
+    console.log(
+      'Ma cookiee "' + cookies.name + '" will expire -> ' + expireDate
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +35,10 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <button onClick={onChange}>Set Cookie</button>
+          {cookies.name && <h3>Hello {cookies.name}!</h3>}
+        </div>
       </header>
     </div>
   )
